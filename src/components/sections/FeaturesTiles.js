4830d6876df40f449097d0010be92aebc42426e8
MatteoLibrizzi <matteo.librizzi@pdp.eu.com>
2022-08-22 14:45:00 +0200

    first commit

---
+++ src/components/sections/FeaturesTiles.js
@@ -0,0 +1,183 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionTilesProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+
+const propTypes = {
+  ...SectionTilesProps.types
+}
+
+const defaultProps = {
+  ...SectionTilesProps.defaults
+}
+const FeaturesTiles = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'features-tiles section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'features-tiles-inner section-inner pt-0',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const tilesClasses = classNames(
+    'tiles-wrap center-content',
+    pushLeft && 'push-left'
+  );
+
+  const sectionHeader = {
+    title: 'Get to know me',
+    paragraph: 'Learn more about my life, job, adventures, challenges, hobbies and passions'
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-01.svg')}
+                      alt="Features tile icon 01"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Computer Science Student
+                    </h4>
+                  <p className="m-0 text-sm">
+                    Studying computer science at <a href="https://www.unipa.it/">Università degli Studi di Palermo</a>
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-02.svg')}
+                      alt="Features tile icon 02"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Software developer
+                    </h4>
+                  <p className="m-0 text-sm">
+                    Working part time as a software developer at <a href="https://www.pdp.eu.com/">PDP Tanuki</a>
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-03.svg')}
+                      alt="Features tile icon 03"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Erasmus Student
+                    </h4>
+                  <p className="m-0 text-sm">
+                    Currently in erasmus in Paris, studying at <a href="https://www.univ-gustave-eiffel.fr/">Université Gustave Eiffel</a>
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-04.svg')}
+                      alt="Features tile icon 04"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Athlete
+                    </h4>
+                  <p className="m-0 text-sm">
+                    I love every sport and do a bit of everything (olympic weightlifting, crossfit, running, surfing, cycling, swimming...)
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-04.svg')}
+                      alt="Features tile icon 04"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Languages
+                    </h4>
+                  <p className="m-0 text-sm">
+                    I love learning languages, I'm currently fluent in italian, english, spanish, and am learning french
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+FeaturesTiles.propTypes = propTypes;
+FeaturesTiles.defaultProps = defaultProps;
+
+export default FeaturesTiles;
