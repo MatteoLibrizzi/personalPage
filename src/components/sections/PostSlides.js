32f7b5e72991a1e151d123c26541b08f47d21c1e
MatteoLibrizzi <matteo.librizzi@pdp.eu.com>
2023-10-02 16:09:33 +0200

    reference to master for split

--- src/components/sections/PostSlides.js
+++ src/components/sections/PostSlides.js
@@ -131,15 +131,56 @@ const PostSlides = ({
                     const {date, title, s3Key, imageData} = post
                     console.log(date, title, s3Key, imageData)
 
+                    /*
+                    <div className="split-item">
+                        <div className={left?"split-item-content center-content-mobile reveal-from-left":"split-item-content center-content-mobile reveal-from-right"} data-reveal-container=".split-item">
+                            <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                            {post.date}
+                            </div>
+                            <h3 className="mt-0 mb-12">
+                            {post.title}
+                            </h3>
+                            {
+                              post.noPreview === 1 ? 
+                                <div>
+                                  <Link to={"/post/" + dateArray[0] + "/" + dateArray[1] + "/" + dateArray[2]}>Click to read</Link>
+                                </div> 
+                                  :
+                                <div>
+                                <p className="m-0">
+                                {post.content.substring(0,300)}...
+                                </p>
+                                <Link to={"/post/" + dateArray[0] + "/" + dateArray[1] + "/" + dateArray[2]}>Keep reading</Link>
+                                </div>
+                            }
+                          </div>
+                        <div className={
+                            classNames(
+                            'split-item-image center-content-mobile reveal-from-bottom',
+                            imageFill && 'split-item-image-fill'
+                            )}
+                            data-reveal-container=".split-item">
+                            { displayImg && <Image
+                            src={require('./../../assets/images/' + dateArray[0] + "-" + dateArray[1] + "-" + dateArray[2] + ".jpg")}
+                            alt={post.title + " - Image"}
+                            width={528}
+                            height={396} />
+                }
+                        </div>
+                        </div>
+                    */
+
                     return (
                       <div key={index} className="split-item">
-                        <div className={"split-item-content center-content-mobile is-revealed"} data-reveal-container=".split-item">
+                        <div className={"split-item-content center-content-mobile reveal-from-left"} data-reveal-container=".split-item">
                         <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
                             {date}
                             </div>
                           <h3 className="mt-0 mb-12">{title}</h3>
                           <div>
-                          <Link to={"/post/" + s3Key}>Click to read</Link>
+                            <div>
+                              <Link to={"/post/" + s3Key}>Click to read</Link>
+                            </div>
                           </div>
                         </div>
                         <div className={
